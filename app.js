f7cc6ba5112fd3dd4e2b0ae950518e264430dbee
Parneet <parneetkaur1568@gmail.com>
2025-11-07 16:21:33 +0530

    Initial commit - ToDo app

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const port = 8080;
+
+app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
+
+let tasks = [];
+
+app.get('/', (req, res) => {
+  res.render('index', { tasks });
+});
+
+app.post('/add', (req, res) => {
+  const task = req.body.task;
+  if (task) tasks.push(task);
+  res.redirect('/');
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
